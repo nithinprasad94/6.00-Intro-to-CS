30de63c902181e5579685e80fefac1c131d6015b
nithinprasad94 <nithinprasad94@gmail.com>
2016-08-31 18:18:28 -0400

    Add files via upload

---
+++ Week-1/600_ps0.py
@@ -0,0 +1,11 @@
+#Name: Nithin Ganesh Prasad
+#Date: 14/05/2016
+#Program Name: 600_ps0.py
+#Program Description: This program takes in a user's date of birth and name
+# and prints it back to the user in format.
+
+print("Enter your date of birth:")
+date_of_birth = raw_input()
+print("Enter your last name:")
+last_name = raw_input()
+print(last_name + " " + date_of_birth)
